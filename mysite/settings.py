9f15a45c486f8e84452bb7c2410f21be4403b6ec
anthony kugel <anthonykugel@gmail.com>
2022-10-10 18:36:53 -0500

    fixed settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,8 +76,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
-Database
-https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
