ef1bfa755a409887d64ae6262c2a31e7e7130710
anthony kugel <anthonykugel@gmail.com>
2023-04-04 08:44:50 -0500

    setup github workflow

--- blog/custom_azure.py
+++ blog/custom_azure.py
@@ -3,13 +3,13 @@ from storages.backends.azure_storage import AzureStorage
 import os
 
 class AzureMediaStorage(AzureStorage):
-    account_name = 'djangostoracc1234' # Must be replaced by your <storage_account_name>
+    account_name = os.environ.get('AZURE_STORAGE_ACCOUNT','')# Must be replaced by your <storage_account_name>
     account_key = os.environ.get('BLOB_MEDIA_KEY','')
     azure_container = 'media'
     expiration_secs = None
 
 class AzureStaticStorage(AzureStorage):
-    account_name = 'djangostoracc1234' # Must be replaced by your storage_account_name
+    account_name = os.environ.get('AZURE_STORAGE_ACCOUNT','') # Must be replaced by your storage_account_name
     account_key = os.environ.get('BLOB_STATIC_KEY','')
     azure_container = 'static'
     expiration_secs = None
