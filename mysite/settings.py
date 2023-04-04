00d5aaef1b7467ac94b957d6039ac600bd85b580
anthony kugel <anthonykugel@gmail.com>
2023-04-04 10:32:57 -0500

    removed storage account from settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -153,7 +153,7 @@ STATICFILES_STORAGE = 'blog.custom_azure.AzureStaticStorage'
 STATIC_LOCATION = "static"
 MEDIA_LOCATION = "media"
 
-AZURE_ACCOUNT_NAME = "djangostoracc1234"
+AZURE_ACCOUNT_NAME = os.environ.get('AZURE_STORAGE_ACCOUNT', '')
 AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
 STATIC_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
 MEDIA_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{MEDIA_LOCATION}/'
