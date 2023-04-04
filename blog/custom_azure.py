3f3b38cf700af46f0410fa76fd69618d35960014
anthony kugel <anthonykugel@gmail.com>
2023-04-04 14:53:29 -0500

    renamed environment variables

--- blog/custom_azure.py
+++ blog/custom_azure.py
@@ -4,12 +4,12 @@ import os
 
 class AzureMediaStorage(AzureStorage):
     account_name = os.environ.get('AZURE_STORAGE_ACCOUNT','')# Must be replaced by your <storage_account_name>
-    account_key = os.environ.get('BLOB_MEDIA_KEY','')
+    account_key = os.environ.get('BLOB_KEY','')
     azure_container = 'media'
     expiration_secs = None
 
 class AzureStaticStorage(AzureStorage):
     account_name = os.environ.get('AZURE_STORAGE_ACCOUNT','') # Must be replaced by your storage_account_name
-    account_key = os.environ.get('BLOB_STATIC_KEY','')
+    account_key = os.environ.get('BLOB_KEY','')
     azure_container = 'static'
     expiration_secs = None
